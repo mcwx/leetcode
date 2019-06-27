665b0c4dd49a886dd7da97d36afe30dc60d80554
houpeng <houpeng@bw30.com>
2019-06-27 10:16:34 +0800

    LeetCode38 countAndSay

--- src/com/leetcode/entrance/HelloWorld.java
+++ src/com/leetcode/entrance/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.leetcode.entrance;
 
-import static com.leetcode.simple.LeetCode53.maxSubArray;
+import com.leetcode.simple.LeetCode38;
+import com.leetcode.simple.LeetCode53;
+
 
 /**
  * @Description:算法校验入口
@@ -11,7 +13,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to leetCode world!");
         try {
-            System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+            System.out.println(LeetCode53.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+            System.out.println(LeetCode38.countAndSay(10));
         } catch (Exception e) {
             e.printStackTrace();
         }
