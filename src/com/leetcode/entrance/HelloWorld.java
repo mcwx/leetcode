c1ab853a176ee3ec77830ed1a75ef670738cae96
houpeng <houpeng@bw30.com>
2019-07-24 17:23:42 +0800

    LeetCode67 addBinary

--- src/com/leetcode/entrance/HelloWorld.java
+++ src/com/leetcode/entrance/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.leetcode.entrance;
 
-import com.leetcode.simple.LeetCode66;
+import com.leetcode.simple.LeetCode67;
 
 
 /**
@@ -16,10 +16,12 @@ public class HelloWorld {
 //            System.out.println(LeetCode38.countAndSay(10));
 //            System.out.println(LeetCode35.searchInsert(new int[]{1, 3, 5, 6}, 5));
 //            System.out.println(LeetCode58.lengthOfLastWord("Hello World"));
-            int[] result = LeetCode66.plusOne(new int[]{9,0,9,9});
-            for (int i:result) {
-                System.out.println(i);
-            }
+//            int[] result = LeetCode66.plusOne(new int[]{9,0,9,9});
+//            for (int i:result) {
+//                System.out.println(i);
+//            }
+            System.out.println(LeetCode67.addBinary("10101101","101001110"));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/com/leetcode/simple/LeetCode67.java
@@ -0,0 +1,22 @@
+package com.leetcode.simple;
+
+/**
+ * @Description:
+ * @Auther: houpeng
+ * @Date: 2019-07-23
+ */
+public class LeetCode67 {
+    public static String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int ca = 0;
+        for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            ans.append(sum % 2);
+            ca = sum / 2;
+        }
+        ans.append(ca == 1 ? ca : "");
+        return ans.reverse().toString();
+    }
+}
