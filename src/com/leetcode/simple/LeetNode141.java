5733d94a89fd958c1ebd776bbd7d4a05bf2da802
houpeng <houpeng@bw30.com>
2019-08-26 17:10:59 +0800

    optimize LeetNode141

--- src/com/leetcode/simple/LeetNode141.java
+++ src/com/leetcode/simple/LeetNode141.java
@@ -4,23 +4,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * @Description:给定一个链表，判断链表中是否有环。
- * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * @Description:给定一个链表，判断链表中是否有环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
  * 示例 1：
  * 输入：head = [3,2,0,-4], pos = 1
  * 输出：true
  * 解释：链表中有一个环，其尾部连接到第二个节点。
- *
+ * <p>
  * 示例 2：
  * 输入：head = [1,2], pos = 0
  * 输出：true
  * 解释：链表中有一个环，其尾部连接到第一个节点。
- *
+ * <p>
  * 示例 3：
  * 输入：head = [1], pos = -1
  * 输出：false
  * 解释：链表中没有环。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/linked-list-cycle
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -31,16 +30,17 @@ public class LeetNode141 {
 
     /**
      * 快慢指针相遇法
+     *
      * @param head
      * @return
      */
     public static boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while (slow != null && fast != null && fast.next != null){
+        while (slow != null && fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(fast == slow){
+            if (fast == slow) {
                 return true;
             }
         }
@@ -49,16 +49,17 @@ public class LeetNode141 {
 
     /**
      * set法
+     *
      * @param head
      * @return
      */
     public static boolean hasCycle1(ListNode head) {
         Set<ListNode> set = new HashSet<>();
-        while (head != null && head.next != null){
-            if(set.contains(head)){
+        while (head != null) {
+            boolean flag = set.add(head);
+            if (flag == false) {
                 return true;
             }
-            set.add(head);
             head = head.next;
         }
         return false;
