65cce260cb553d59d7915760cadca7aa8ca2aa87
houpeng <houpeng@bw30.com>
2019-08-30 15:10:01 +0800

    LeetCode20 isValid

---
+++ src/com/leetcode/simple/LeetCode20.java
@@ -0,0 +1,63 @@
+package com.leetcode.simple;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * @Description:给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ * 输入: "()"
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: "()[]{}"
+ * 输出: true
+ *
+ * 示例 3:
+ * 输入: "(]"
+ * 输出: false
+ *
+ * 示例 4:
+ * 输入: "([)]"
+ * 输出: false
+ *
+ * 示例 5:
+ * 输入: "{[]}"
+ * 输出: true
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Auther: houpeng
+ * @Date: 2019-08-29
+ */
+public class LeetCode20 {
+    public static boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if (map.containsValue(c)) {
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                if (stack.isEmpty() || !stack.peek().equals(map.get(c))) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(LeetCode20.isValid("()[]{}"));
+    }
+}
