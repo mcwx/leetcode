b0a4bd5f9f643bf66d22e640e3bdba04cf94b744
houpeng <houpeng@bw30.com>
2020-05-04 10:37:45 +0800

    LeetCode566

---
+++ src/com/leetcode/simple/LeetCode566.java
@@ -0,0 +1,68 @@
+package com.leetcode.simple;
+
+import java.util.Arrays;
+
+/**
+ * @Description:重塑矩阵 在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+ * 给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。
+ * 重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+ * 如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+ * <p>
+ * 示例 1:
+ * 输入:
+ * nums =
+ * [[1,2],
+ * [3,4]]
+ * r = 1, c = 4
+ * 输出:
+ * [[1,2,3,4]]
+ * 解释:
+ * 行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+ * <p>
+ * 示例 2:
+ * 输入:
+ * nums =
+ * [[1,2],
+ * [3,4]]
+ * r = 2, c = 4
+ * 输出:
+ * [[1,2],
+ * [3,4]]
+ * 解释:
+ * 没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reshape-the-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Auther: houpeng
+ * @Date: 2020/5/3
+ */
+public class LeetCode566 {
+    public static void main(String[] args) {
+        int[][] nums = new int[][]{{1, 2}, {3, 4}};
+        int[][] result = matrixReshape(nums, 1, 4);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[][] matrixReshape(int[][] nums, int r, int c) {
+        if (nums.length == 0 || nums.length * nums[0].length != r * c) {
+            return nums;
+        }
+        int[][] result = new int[r][c];
+        int rCount = 0;
+        int cCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                int value = nums[i][j];
+                if (cCount == c) {
+                    rCount++;
+                    cCount = 0;
+                }
+                result[rCount][cCount] = value;
+                cCount++;
+            }
+        }
+        return result;
+    }
+}
