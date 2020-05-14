6091170080d599d2dd4056f009b54b7406b94ec9
houpeng <houpeng@bw30.com>
2020-05-14 20:45:43 +0800

    LeetCode94

--- src/com/leetcode/medium/LeetCode94.java
+++ src/com/leetcode/medium/LeetCode94.java
@@ -1,19 +1,19 @@
 package com.leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * @Description:给定一个二叉树，返回它的中序 遍历。
- * 示例:
+ * @Description:给定一个二叉树，返回它的中序 遍历。 示例:
  * 输入: [1,null,2,3]
- *    1
- *     \
- *      2
- *     /
- *    3
+ * 1
+ * \
+ * 2
+ * /
+ * 3
  * 输出: [1,3,2]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -47,5 +47,27 @@ public class LeetCode94 {
             list.add(node.val);
             helper(node.right, list);
         }
+    }
+
+
+    public static List<Integer> inorderTraversal_01(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
+
+        }
+        return result;
     }
 }
