9c5210a9d47009a123b71cbb79af0d548edc4269
houpeng <houpeng@bw30.com>
2020-05-15 18:10:12 +0800

    LeetCode104

--- src/com/leetcode/simple/LeetCode104.java
+++ src/com/leetcode/simple/LeetCode104.java
@@ -4,21 +4,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * @Description:    二叉树的最大深度
+ * @Description: 二叉树的最大深度
  * 给定一个二叉树，找出其最大深度。
- *
+ * <p>
  * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
- *
+ * <p>
  * 说明: 叶子节点是指没有子节点的节点。
- *
+ * <p>
  * 示例：
  * 给定二叉树 [3,9,20,null,null,15,7]，
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
  * 返回它的最大深度 3 。
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
@@ -36,6 +36,7 @@ public class LeetCode104 {
 
     /**
      * BFS，广度优先算法
+     *
      * @param root
      * @return
      */
@@ -43,37 +44,37 @@ public class LeetCode104 {
         if (root == null) {
             return 0;
         }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
         int depth = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
+        while (!queue.isEmpty()) {
+            depth++;
+            int size = queue.size();
             for (int i = 0; i < size; i++) {
-                TreeNode poll = q.poll();
-                if (poll.left != null) {
-                    q.offer(poll.left);
+                TreeNode currLevel = queue.poll();
+                if (currLevel.left != null) {
+                    queue.add(currLevel.left);
                 }
-                if (poll.right != null) {
-                    q.offer(poll.right);
+                if (currLevel.right != null) {
+                    queue.add(currLevel.right);
                 }
             }
-            depth++;
         }
         return depth;
     }
 
     /**
      * DFS，深度优先算法，递归
+     *
      * @param root
      * @return
      */
     public int maxDepth1(TreeNode root) {
         if (root == null) {
             return 0;
-        } else {
-            int leftDepth = maxDepth(root.left);
-            int righDepth = maxDepth(root.right);
-            return Math.max(leftDepth, righDepth) + 1;
         }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
