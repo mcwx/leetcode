116c36e0a94b66eb0f747345591306eb6d0f1508
houpeng <houpeng@bw30.com>
2020-05-17 10:26:13 +0800

    LeetCode28

--- src/com/leetcode/simple/LeetCode28.java
+++ src/com/leetcode/simple/LeetCode28.java
@@ -14,13 +14,45 @@ package com.leetcode.simple;
  */
 public class LeetCode28 {
 
+    public static void main(String[] args) {
+        String haystack = "a";
+        String needle = "a";
+        System.out.println(strStr(haystack, needle));
+    }
+
     /**
      * 使用封装好的方法实现功能（是否是偷懒呢？）
+     *
      * @param haystack
      * @param needle
      * @return
      */
     public static int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        return haystack.indexOf("");
+    }
+
+    public static int strStr_01(String haystack, String needle) {
+//        if (haystack == null) {
+//            return -1;
+//        }
+        if (needle == "") {
+            return 0;
+        }
+        char[] charsHaystack = haystack.toCharArray();
+        char[] charsNeedle = needle.toCharArray();
+        int j = 0;
+        for (int i = 0; i < charsHaystack.length - charsNeedle.length + 1; i++) {
+            int k = i;
+            while (j < charsNeedle.length && charsHaystack[k] == charsNeedle[j]) {
+                k++;
+                j++;
+            }
+            if (j == charsNeedle.length) {
+                return i;
+            } else {
+                j = 0;
+            }
+        }
+        return -1;
     }
 }
