36facf2fc4eb0ea93df051020d3e9eae0b8fd695
houpeng <houpeng@bw30.com>
2020-05-19 11:58:30 +0800

    LeetCode155

---
+++ src/com/leetcode/simple/LeetCode155.java
@@ -0,0 +1,63 @@
+package com.leetcode.simple;
+
+import java.util.Stack;
+
+/**
+ * @Description:最小栈 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * push(x) —— 将元素 x 推入栈中。
+ * pop() —— 删除栈顶的元素。
+ * top() —— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ * 示例:
+ * 输入：
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * 输出：
+ * [null,null,null,null,-3,null,0,-2]
+ * 解释：
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Auther: houpeng
+ * @Date: 2020/5/19
+ */
+public class LeetCode155 {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> min_stack;
+
+    public LeetCode155() {
+        stack = new Stack<>();
+        min_stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (min_stack.isEmpty() || x <= min_stack.peek()) {
+            min_stack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(min_stack.peek())) {
+            min_stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min_stack.peek();
+    }
+
+}
