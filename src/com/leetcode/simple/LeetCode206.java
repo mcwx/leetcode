1c3477f83eaa37e137b980cf2ad7998a9b400579
houpeng <houpeng@bw30.com>
2020-05-19 17:24:23 +0800

    LeetCode206

--- src/com/leetcode/simple/LeetCode206.java
+++ src/com/leetcode/simple/LeetCode206.java
@@ -9,45 +9,8 @@ package com.leetcode.simple;
  * @Date: 2019-08-22
  */
 public class LeetCode206 {
-    /**
-     * 迭代法
-     * @param head
-     * @return
-     */
-    public static ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
 
-    /**
-     * 递归法
-     * @param head
-     * @return
-     */
-    public static ListNode reverseList1(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode p = reverseList1(head.next);
-        head.next.next = head;
-        head.next = null;
-        return p;
-    }
-
-    public static void main(String[] args) {
-        ListNode ln = new ListNode(1);
-        ln.next = new ListNode(2);
-        ln.next.next = new ListNode(3);
-        System.out.println(reverseList1(ln));
-    }
-
-
-    public static class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
 
@@ -70,5 +33,47 @@ public class LeetCode206 {
             }
             return sb.toString();
         }
+    }
+
+    public static void main(String[] args) {
+        ListNode ln = new ListNode(1);
+        ln.next = new ListNode(2);
+        ln.next.next = new ListNode(3);
+        System.out.println(reverseList_01(ln));
+    }
+
+    /**
+     * 迭代法
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    /**
+     * 递归法
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList_01(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList_01(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
