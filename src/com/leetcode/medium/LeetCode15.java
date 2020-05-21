5ee48f267ead875f122adb8d403f45d21f3c502c
houpeng <houpeng@bw30.com>
2020-05-21 11:51:05 +0800

    LeetCode15

--- src/com/leetcode/medium/LeetCode15.java
+++ src/com/leetcode/medium/LeetCode15.java
@@ -3,15 +3,15 @@ package com.leetcode.medium;
 import java.util.*;
 
 /**
- * @Description:给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+ * @Description:三数之和 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
  * 注意：答案中不可以包含重复的三元组。
  * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
  * 满足要求的三元组集合为：
  * [
- *   [-1, 0, 1],
- *   [-1, -1, 2]
+ * [-1, 0, 1],
+ * [-1, -1, 2]
  * ]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/3sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -21,47 +21,44 @@ import java.util.*;
 public class LeetCode15 {
 
 
-
     public static void main(String[] args) {
 
-        int[] test = new int[]{1,-1,-1,0};
+        int[] test = new int[]{-1, 0, 1, 2, -1, -4};
         List<List<Integer>> lists = threeSum(test);
         System.out.println(lists);
     }
 
     /**
      * 双指针法
+     *
      * @param nums
      * @return
      */
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        if (nums == null || nums.length < 3) {
-            return result;
-        }
-        int len = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < len - 1; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i] > 0) {
                 break;
             }
+            int l = i + 1;
+            int r = nums.length - 1;
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int L = i + 1;
-            int R = len - 1;
-            while (L < R) {
-                int num = nums[i] + nums[L] + nums[R];
-                if (0 == num) {
-                    result.add(Arrays.asList(nums[i], nums[L], nums[R]));
-                    while (L < R && nums[L] == nums[L + 1]) L++;
-                    while (L < R && nums[R] == nums[R - 1]) R--;
-                    L++;
-                    R--;
-                } else if (num < 0) {
-                    L++;
-                } else if (num > 0) {
-                    R--;
+
+            while (l < r) {
+                int currSum = nums[i] + nums[l] + nums[r];
+                if (currSum < 0) {
+                    l++;
+                } else if (currSum > 0) {
+                    r--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
                 }
             }
         }
