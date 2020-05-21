c625bc1c0e93dfc822800eadb406c5de536d3b1b
houpeng <houpeng@bw30.com>
2020-05-21 14:29:30 +0800

    LeetCode695

---
+++ src/com/leetcode/medium/LeetCode695.java
@@ -0,0 +1,51 @@
+package com.leetcode.medium;
+
+/**
+ * @Description:岛屿的最大面积 给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+ * 一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
+ * 示例 1:
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。
+ * 示例 2:
+ * [[0,0,0,0,0,0,0,0]]
+ * 对于上面这个给定的矩阵, 返回 0。
+ * 注意: 给定的矩阵grid 的长度和宽度都不超过 50。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/max-area-of-island
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Auther: houpeng
+ * @Date: 2020/5/21
+ */
+public class LeetCode695 {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                maxArea = Math.max(maxArea, dfs(i, j, grid));
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        int area = 1;
+        area += dfs(i + 1, j, grid);
+        area += dfs(i - 1, j, grid);
+        area += dfs(i, j + 1, grid);
+        area += dfs(i, j - 1, grid);
+
+        return area;
+    }
+}
