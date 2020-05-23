a6b52b30736383ad95c4aa7d8a0802b3ce997643
houpeng <houpeng@bw30.com>
2020-05-23 17:36:44 +0800

    LeetCode1

--- src/com/leetcode/simple/LeetCode1.java
+++ src/com/leetcode/simple/LeetCode1.java
@@ -1,6 +1,7 @@
 package com.leetcode.simple;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * @Description:给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -9,7 +10,7 @@ import java.util.HashMap;
  * 给定 nums = [2, 7, 11, 15], target = 9
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -36,7 +37,7 @@ public class LeetCode1 {
      * @param target
      * @return
      */
-    public static int[] twoSum1(int[] nums, int target) {
+    public static int[] twoSum_01(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (target - nums[j] == nums[i]) {
@@ -46,5 +47,4 @@ public class LeetCode1 {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
-
 }
