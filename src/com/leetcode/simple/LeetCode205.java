d50005eb62c37c781db87286d3a7d14afbb13d7e
houpeng <houpeng@bw30.com>
2020-05-24 10:37:53 +0800

    LeetCode205

---
+++ src/com/leetcode/simple/LeetCode205.java
@@ -0,0 +1,51 @@
+package com.leetcode.simple;
+
+import java.util.HashMap;
+
+/**
+ * @Description:同构字符串 给定两个字符串 s 和 t，判断它们是否是同构的。
+ * 如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+ * 所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+ * 示例 1:
+ * 输入: s = "egg", t = "add"
+ * 输出: true
+ * 示例 2:
+ * 输入: s = "foo", t = "bar"
+ * 输出: false
+ * 示例 3:
+ * 输入: s = "paper", t = "title"
+ * 输出: true
+ * 说明:
+ * 你可以假设 s 和 t 具有相同的长度。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/isomorphic-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Auther: houpeng
+ * @Date: 2020/5/23
+ */
+public class LeetCode205 {
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("ab", "aa"));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        return isIsomorphicHelper(s, t) && isIsomorphicHelper(t, s);
+    }
+
+    private static boolean isIsomorphicHelper(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (map.containsKey(sChar) && map.get(sChar) != tChar) {
+                return false;
+            } else {
+                map.put(sChar, tChar);
+            }
+        }
+        return true;
+    }
+}
