03a9b2c7a30a347f90cd2e52938e4e04adacb9ca
houpeng <houpeng@bw30.com>
2020-05-29 19:18:51 +0800

    LeetCode15

--- src/com/leetcode/medium/LeetCode15.java
+++ src/com/leetcode/medium/LeetCode15.java
@@ -11,7 +11,6 @@ import java.util.*;
  * [-1, 0, 1],
  * [-1, -1, 2]
  * ]
- * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/3sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -23,8 +22,8 @@ public class LeetCode15 {
 
     public static void main(String[] args) {
 
-        int[] test = new int[]{-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> lists = threeSum(test);
+        int[] test = new int[]{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+        List<List<Integer>> lists = threeSum_01(test);
         System.out.println(lists);
     }
 
@@ -65,4 +64,86 @@ public class LeetCode15 {
         return result;
     }
 
+    public static List<List<Integer>> threeSum_01(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int currSum = nums[i] + nums[left] + nums[right];
+                if (currSum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (currSum < 0) {
+                    left++;
+                } else if (currSum > 0) {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 抄的答案
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum_02(int[] nums) {
+        if (nums == null || nums.length <= 2) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 加速1：c为非负数，就不能满足a+b+c=0了
+            if (nums[i] > 0) {
+                return result;
+            }
+            // 加速2：跳过计算过的数据，同时防止结果重复
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int head = i + 1;
+            int tail = nums.length - 1;
+            while (head < tail) {
+                int sum = -(nums[head] + nums[tail]);
+                if (sum == nums[i]) {
+                    result.add(Arrays.asList(nums[i], nums[head], nums[tail]));
+                    // 加速3：跳过计算过的数据，同时防止结果重复
+                    while (head < tail && nums[head] == nums[head + 1]) {
+                        head++;
+                    }
+                    while (head < tail && nums[tail] == nums[tail - 1]) {
+                        tail--;
+                    }
+                }
+                if (sum <= nums[i]) {
+                    tail--;
+                } else {
+                    head++;
+                }
+            }
+        }
+        return result;
+    }
 }
