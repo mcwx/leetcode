1c78a9471f17359b0ffb05640ce4e208d5f12c6e
houpeng <houpeng@bw30.com>
2020-06-03 22:58:12 +0800

    LeetCode14

--- src/com/leetcode/simple/LeetCode14.java
+++ src/com/leetcode/simple/LeetCode14.java
@@ -32,9 +32,25 @@ public class LeetCode14 {
         }
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) == -1) {
+            while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix_01(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if ("".equals(prefix)) {
                     return "";
                 }
             }
