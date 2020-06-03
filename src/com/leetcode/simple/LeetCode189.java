b75a448f2da0878c7d97cb42fb62d4792c478074
houpeng <houpeng@bw30.com>
2020-06-03 21:19:13 +0800

    LeetCode189

--- src/com/leetcode/simple/LeetCode189.java
+++ src/com/leetcode/simple/LeetCode189.java
@@ -2,7 +2,6 @@ package com.leetcode.simple;
 
 /**
  * @Description:旋转数组 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
- * <p>
  * 示例 1:
  * 输入: [1,2,3,4,5,6,7] 和 k = 3
  * 输出: [5,6,7,1,2,3,4]
@@ -10,7 +9,6 @@ package com.leetcode.simple;
  * 向右旋转 1 步: [7,1,2,3,4,5,6]
  * 向右旋转 2 步: [6,7,1,2,3,4,5]
  * 向右旋转 3 步: [5,6,7,1,2,3,4]
- * <p>
  * 示例 2:
  * 输入: [-1,-100,3,99] 和 k = 2
  * 输出: [3,99,-1,-100]
@@ -18,10 +16,8 @@ package com.leetcode.simple;
  * 向右旋转 1 步: [99,-1,-100,3]
  * 向右旋转 2 步: [3,99,-1,-100]
  * 说明:
- * <p>
  * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
  * 要求使用空间复杂度为 O(1) 的 原地 算法。
- * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/rotate-array
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -29,11 +25,6 @@ package com.leetcode.simple;
  * @Date: 2020/4/30
  */
 public class LeetCode189 {
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{1,2,3,4,5,6,7};
-        rotate_2(nums,3);
-    }
 
     /**
      * 暴力法
@@ -69,24 +60,23 @@ public class LeetCode189 {
         }
     }
 
-    /**
-     * 使用翻转
-     *
-     * @param nums
-     * @param k
-     */
-    public static void rotate_2(int[] nums, int k) {
+    public void rotate_02(int[] nums, int k) {
         k = k % nums.length;
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
         reverse(nums, k, nums.length - 1);
     }
 
-    private static void reverse(int[] array, int start, int end) {
+    private void reverse(int[] nums, int start, int end) {
         while (start < end) {
-            int tmp = array[start];
-            array[start] = array[end];
-            array[end] = tmp;
+            //算术运算交换位置
+            nums[start] = nums[end] - nums[start];
+            nums[end] = nums[end] - nums[start];
+            nums[start] = nums[end] + nums[start];
+            //位运算交换位置
+//            nums[start] = nums[start] ^ nums[end];
+//            nums[end] = nums[start] ^ nums[end];
+//            nums[start] = nums[start] ^ nums[end];
             start++;
             end--;
         }
